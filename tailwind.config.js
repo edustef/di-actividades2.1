1b4ae2bd37af9a6154bd6556fe1d7aa07541205d
Eduard <eduardstefan290@gmail.com>
2021-01-08 19:36:05 +0100

    for remote

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: { mode: "layers", content: ["./public/**/*.html"] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      merri: ["Merriweather", "serif"],
+      lora: ["Lora", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        color1: "#283c63",
+        color2: "#928a97",
+        color3: "#fbe8d3",
+        color4: "#f85f73",
+      },
+    },
   },
   variants: {},
   plugins: [],
